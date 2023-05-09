f57dc58b2d13ea53f71ae3b8be292b5725a24ebd
Harrison <Harrison@fanruan.com>
2023-05-09 14:08:36 +0800

    feature: 更新一版提示语

--- app/api/prompts/helper-message.ts
+++ app/api/prompts/helper-message.ts
@@ -74,17 +74,15 @@ export class HelperMessage implements MessageMaker {
 
     let context: MessageContext = { sources };
 
-    const systemContent = `你是一个严谨、精明、注重格式、表达详细的助手。
-  当给你 CONTEXT 时，你只用这些信息来回答问题。
-  你以 markdown 的形式输出。如果有代码片段，那么就输出为代码格式。
-  如果有多个步骤或者需要说明多个信息，就用 1- 2- 3- 这样的形式输出。
-  如果你不确定回答，且答案没有明确写在提供的 CONTEXT 中，你就在回答开头说:"对不起，我不知道如何帮助你。" 
-  注意，不要输出null, 不要编造URL`;
+    const systemContent =
+      `You are a helpful assistant. When given CONTEXT you answer questions using only that information,` +
+      `and you always format your output in markdown. You include code snippets if relevant. ` +
+      `If you are unsure and the answer is not explicitly written in the CONTEXT provided, you must say: "对不起，我不知道如何帮助你。"`;
 
     const userContent = `CONTEXT:
-  Next.js是一个React框架，用于创建网络应用。
+  """Next.js是一个React框架，用于创建网络应用。
   TITLE: next.js官网
-  SOURCE: nextjs.org/docs/faq
+  SOURCE: nextjs.org/docs/faq"""
   
   QUESTION: 
   what is nextjs?

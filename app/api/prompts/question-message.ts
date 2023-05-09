a88b6238c6a8d3bd7285a9b3d5e4efd6e1ce2bcf
Harrison <Harrison@fanruan.com>
2023-05-09 14:08:50 +0800

    Revert "feature: 更新一版提示语"
    This reverts commit f57dc58b2d13ea53f71ae3b8be292b5725a24ebd.

--- app/api/prompts/question-message.ts
+++ app/api/prompts/question-message.ts
@@ -70,13 +70,15 @@ export class QuestionMessage implements MessageMaker {
 
     let context: MessageContext = { sources };
 
-    const systemContent =
-      `You are a helpful assistant. When given CONTEXT you answer questions using only that information,` +
-      `and you always format your output in markdown. You include code snippets if relevant. ` +
-      `If you are unsure and the answer is not explicitly written in the CONTEXT provided, you must say: "对不起，我不知道如何帮助你。"`;
+    const systemContent = `你是一个严谨、精明、注重格式、表达详细的助手。
+  你只用提供的 CONTEXT 来回答问题，不要捏造事实。
+  如果你不确定回答，你就在回答开头说:"对不起，我不知道如何帮助你。"。
+  以 markdown 的形式输出。如果有代码片段，那么就输出为代码格式。
+  如果有多个步骤或者需要说明多个信息，就用 1- 2- 3- 这样的形式输出。
+  `;
 
     const userContent = `CONTEXT:
-  """Next.js是一个React框架，用于创建网络应用。"""
+  Next.js是一个React框架，用于创建网络应用。
 
   QUESTION:
   what is nextjs?
@@ -93,7 +95,7 @@ export class QuestionMessage implements MessageMaker {
     const queryMessage: Message = {
       role: "user",
       content: `CONTEXT:
-  """${contextText}"""
+  ${contextText}
   
   USER QUESTION: 
   """${userMessage.content}"""
